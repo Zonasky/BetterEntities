1fa0e83d66dabf3c2a63868debe4fe2f37bdec6b
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-09 13:40:00 +1000

    Update Main.php

--- src/ZonaSky/BetterEntities/Main.php
+++ src/ZonaSky/BetterEntities/Main.php
@@ -1,4 +1,15 @@
 <?php
 
+namespace ZonaSky\BetterEntities;
+
 use ZonaSky\BetterEntities\Entities;
-use ZonaSky\BetterEntities\Entities\Zombie;
+use ZonaSky\BetterEntities\Loader;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase Implements Entities {
+
+  public function onLoad(): void {
+$this->saveDefaultConfig();
+    
+
+  }
+}
